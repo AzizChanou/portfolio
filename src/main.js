c342ab83ff7d41143c89b49eb135c71f3f0ba726
Aziz Chanou <azizchanou@gmail.com>
2022-10-02 01:46:16 +0100

    MAJ

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import VueLazyload from 'vue-lazyload'
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -10,7 +9,6 @@ import './style.css'
 library.add(fab)
 
 createApp(App)
-    .use(router)
     .use(VueLazyload, {
         preLoad: 1.3,
         //error: errorimage,
