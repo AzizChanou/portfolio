5e1715e16d2e0dc4074748df47617cb66402657d
Aziz Chanou <azizchanou@gmail.com>
2022-10-02 15:30:25 +0100

    MAJ

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,29 +1,19 @@
 <script setup>
 import emailjs from 'emailjs-com';
+import { ref } from 'vue';
 
-const form = {
-    name: "",
-    email: "",
-    message: "",
-}
+const form = ref(null);
 
-const sendEmail = (e) => {
+const sendMail = () => {
 
-    try {
-        emailjs.sendForm(import.meta.env.VITE_MAIL_SERVICE_ID, import.meta.env.VITE_MAIL_TEMPLATE_ID, e.target,
-            'YOUR_USER_ID', {
-            name: form.name,
-            email: form.email,
-            message: form.message
-        })
+    emailjs.sendForm(import.meta.env.VITE_MAIL_SERVICE_ID, import.meta.env.VITE_MAIL_TEMPLATE_ID, form.value,
+        import.meta.env.VITE_MAIL_PUBLIC_KEY)
+        .then((result) => {
+            alert('Mail envoyé !');
+        }, (error) => {
+            alert('Mail non envoyé, réessayer !');
+        });
 
-    } catch (error) {
-        console.table({ error })
-    }
-
-    form.name = ''
-    form.email = ''
-    form.message = ''
 }
 
 </script>
@@ -31,22 +21,22 @@ const sendEmail = (e) => {
 <template>
     <div class="space-y-8 w-full max-w-screen-md">
         <h3 class="text-center text-3xl font-semibold">Contactez-moi</h3>
-        <form @submit.prevent="sendEmail()" method="post" class="space-y-4 w-full">
+        <form ref="form" @submit.prevent="sendMail" class="space-y-4 w-full">
             <div class="flex flex-col sm:flex-row space-x-0 sm:space-x-4 w-full">
                 <div class="w-full mt-3">
                     <label for="fullname" class="font-semibold text-lg">Nom & prénom.s</label>
-                    <input v-model="form.fullname" id="fullname" type="text"
+                    <input name="name" id="fullname" type="text"
                         class="dark:bg-secondary bg-gray-200 rounded w-full p-4 mt-2" required>
                 </div>
                 <div class="w-full mt-3">
                     <label for="email" class="font-semibold text-lg">Email</label>
-                    <input v-model="form.email" id="email" type="email"
+                    <input name="email" id="email" type="email"
                         class="dark:bg-secondary bg-gray-200 rounded w-full p-4 mt-2" required>
                 </div>
             </div>
             <div class="w-full mt-3">
                 <label for="message" class="font-semibold text-lg">Message</label>
-                <textarea v-model="form.message" id="message" cols="30" rows="10"
+                <textarea name="message" id="message" cols="30" rows="10"
                     class="dark:bg-secondary bg-gray-200 rounded w-full p-4 mt-2" required></textarea>
             </div>
             <button type="submit"
